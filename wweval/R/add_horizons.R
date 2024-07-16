53fd5f00528f7c80ca8c2f590d5477f0057e118e
kaitejohnson <uox1@cdc.gov>
2024-07-16 15:56:13 -0400

    change from weekly to daily horizons

--- wweval/R/add_horizons.R
+++ wweval/R/add_horizons.R
@@ -3,7 +3,7 @@
 #' @description
 #' This function takes in a tibble of quantiles containing the columns
 #' `date`, `forecast_date` and `period` and adds the following columns:
-#' `horizon_weeks` (an integer) and `horizon` (a string to be used for
+#' `horizon_days` (an integer) and `horizon` (a string to be used for
 #' categorical grouping of horizons)
 #'
 #'
@@ -11,27 +11,21 @@
 #' required columns: `date`, `forecast_date`, and `period`
 #'
 #' @return a tibble containing the same columns as `quantiles` plus
-#' `horizon_weeks` and `horizon`
+#' `horizon_days` and `horizon`
 #' @export
 add_horizons_to_quantiles <- function(quantiles) {
   quantiles_w_horizons <- quantiles |>
     dplyr::mutate(
-      # As written, this generates negative integer weeks for days before the
-      # forecast date, and +1 for days after the forecast date, and a 0
-      # on the forecast date
-      horizon_weeks = ifelse(date > forecast_date,
-        ceiling(as.numeric(date - forecast_date) / 7),
-        -ceiling(abs(as.numeric(date - forecast_date) / 7))
-      )
+      horizon_days = as.numeric(date - forecast_date)
     ) |>
     dplyr::mutate(
       horizon = dplyr::case_when(
         period == "calibration" ~ "calibration",
-        period != "calibration" & horizon_weeks < 0 ~ "nowcast",
-        horizon_weeks == 1 ~ "1 wk",
-        horizon_weeks == 2 ~ "2 wks",
-        horizon_weeks == 3 ~ "3 wks",
-        horizon_weeks == 4 ~ "4 wks",
+        period != "calibration" & horizon_days < 0 ~ "nowcast",
+        horizon_days > 0 & horizon_days <= 7 ~ "1 wk",
+        horizon_days > 7 & horizon_days <= 14 ~ "2 wks",
+        horizon_days > 14 & horizon_days <= 21 ~ "3 wks",
+        horizon_days > 21 & horizon_days <= 28 ~ "4 wks",
         TRUE ~ NA_character_
       )
     )
@@ -43,39 +37,34 @@ add_horizons_to_quantiles <- function(quantiles) {
 #' @description
 #' This function takes in a tibble of scores on the nowcasts/forecasts
 #' containing the columns `date` and `forecast_date` and adds the following
-#' columns: `horizon_weeks` (an integer) and `horizon` (a string to be used for
+#' columns: `horizon_days` (an integer) and `horizon` (a string to be used for
 #' categorical grouping of horizons)
 #'
 #'
 #' @param scores A tibble containing the quantiled values and the following
 #' required columns: `date`and `forecast_date`
-#' @param max_weeks_nowcast An integer indicating the expected maximum number
+#' @param max_days_nowcast An integer indicating the expected maximum number
 #' of weeks where date < forecast date. If the output contains values beyond
 #' this threshold, an error will be thrown which may indicate that the scores
 #' contain scores for calibrated data.
 #'
 #' @return a tibble containing the same columns as `scores` plus
-#' `horizon_weeks` and `horizon`
-#' @export
+#' `horizon_days` and `horizon`
+#' @exportmax_days_nowscast
 add_horizons_to_scores <- function(scores,
-                                   max_weeks_nowcast = 2) {
+                                   max_days_nowcast = 14) {
   scores_w_horizons <- scores |>
     dplyr::mutate(
-      # As written, this generates negative integer weeks for days before the
-      # forecast date, and +1 for days after the forecast date, and a 0
-      # on the forecast date
-      horizon_weeks = ifelse(date > forecast_date,
-        ceiling(as.numeric(date - forecast_date) / 7),
-        -ceiling(abs(as.numeric(date - forecast_date) / 7))
-      )
+      horizon_days = as.numeric(date - forecast_date)
     ) |>
     dplyr::mutate(
       horizon = dplyr::case_when(
-        horizon_weeks < 0 ~ "nowcast",
-        horizon_weeks == 1 ~ "1 wk",
-        horizon_weeks == 2 ~ "2 wks",
-        horizon_weeks == 3 ~ "3 wks",
-        horizon_weeks == 4 ~ "4 wks",
+        horizon_days <= -max_days_nowcast ~ "calibration",
+        horizon_days > -max_days_nowcast & horizon_days <= 0 ~ "nowcast",
+        horizon_days > 0 & horizon_days <= 7 ~ "1 wk",
+        horizon_days > 7 & horizon_days <= 14 ~ "2 wks",
+        horizon_days > 14 & horizon_days <= 21 ~ "3 wks",
+        horizon_days > 21 & horizon_days <= 28 ~ "4 wks",
         TRUE ~ NA_character_
       )
     )
