65aea4d763827bddad9e6f90ddc9a48e308fb40b
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2024-08-01 11:52:07 -0400

    Update wweval/R/ms_fig4.R
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- wweval/R/ms_fig4.R
+++ wweval/R/ms_fig4.R
@@ -496,7 +496,7 @@ make_fig4_rel_crps_by_phase <- function(scores) {
   return(p)
 }
 #' Make figure that plots distribution of crps scores stratified by
-#' epidemic phase
+#' epidemic phase but aggregated across horizons
 #'
 #' @param scores A tibble of scores by location, forecast date, date and model,
 #' containing the outputs of `scoringutils::score()` on samples plus metadata
