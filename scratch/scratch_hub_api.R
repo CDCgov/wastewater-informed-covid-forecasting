c681ba8485d6e5363d40c0069289099145e04396
kaitejohnson <uox1@cdc.gov>
2024-08-26 16:36:39 -0400

    add zoltr script to test out

---
+++ scratch/scratch_hub_api.R
@@ -0,0 +1,6 @@
+# Scratch file to prototype using zoltr API instead of pulling into memory from github
+
+library(zoltr)
+
+truth_data_path <- "https://media.githubusercontent.com/media/reichlab/covid19-forecast-hub/master/data-truth/truth-Incident%20Hospitalizations.csv" # nolint
+truth_data <- truth_data <- readr::read_csv(truth_data_path)
