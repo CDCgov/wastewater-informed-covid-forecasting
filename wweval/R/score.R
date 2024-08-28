4f4c1d3a8f916f66bf69515cff0ca096da3e7651
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2024-08-27 21:11:27 -0400

    Update wweval/R/score.R
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- wweval/R/score.R
+++ wweval/R/score.R
@@ -293,7 +293,7 @@ query_and_select_models <- function(prop_dates_for_incl_hub,
       prop_present = n_forecast_dates / !!n_unique_forecasts
     )
 
-  models <- n_forecasts_per_model |>
+  models <- forecasts_present_per_model |>
     dplyr::filter(prop_present > !!prop_dates_for_incl_hub) |>
     dplyr::pull(model)
 
