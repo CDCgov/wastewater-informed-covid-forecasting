8a5a50d949cad3544e868517d60ef3d4dde900c4
Kaitlyn Johnson <uox1@cdc.gov>
2024-10-22 13:56:52 +0000

    delete cfaforecastrenewalww and delay distribs

--- cfaforecastrenewalww/data-raw/example_data.R
+++
@@ -1,7 +0,0 @@
-set.seed(1)
-toy_data_and_params <- cfaforecastrenewalww::generate_simulated_data()
-example_df <- toy_data_and_params$example_df
-param_df <- toy_data_and_params$param_df
-
-usethis::use_data(param_df, overwrite = TRUE)
-usethis::use_data(example_df, overwrite = TRUE)
