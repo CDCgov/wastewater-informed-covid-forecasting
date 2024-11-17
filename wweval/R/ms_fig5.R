e9857d229968722ac89b3e5a7b763553a7c07e0e
Kaitlyn Johnson <uox1@cdc.gov>
2024-11-17 19:21:36 +0000

    add description of time period

--- wweval/R/ms_fig5.R
+++ wweval/R/ms_fig5.R
@@ -1,6 +1,7 @@
 #' Make summary table of WIS scores in Hub models overall
 #'
 #' @param scores quantile based scores from the hub
+#' @param time_period string indicating which time period to make the plot for
 #' @param fig_file_dir directory to save figure
 #'
 #' @return A table with the average scores of each model over the time period
