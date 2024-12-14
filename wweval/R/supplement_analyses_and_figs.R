124979844ed5574c0d7d1027928797268633b87d
Kaitlyn Johnson <uox1@cdc.gov>
2024-12-14 18:08:13 +0000

    change colors on absolute heatmaps

--- wweval/R/supplement_analyses_and_figs.R
+++ wweval/R/supplement_analyses_and_figs.R
@@ -789,7 +789,8 @@ get_plot_hub_perf_heatmap <- function(scores,
   p <- ggplot(scores_summary) +
     geom_tile(aes(x = forecast_date, y = location_name, fill = avg_wis)) +
     scale_fill_gradient(
-      midpoint = mean(scores_summary$avg_wis),
+      low = "white",
+      high = "darkred",
       guide = "colourbar", aesthetics = "fill"
     ) +
     geom_text(aes(
@@ -837,7 +838,8 @@ get_plot_comb_perf_heatmap <- function(scores,
   p <- ggplot(scores_summary) +
     geom_tile(aes(x = forecast_date, y = location, fill = avg_crps)) +
     scale_fill_gradient(
-      midpoint = mean(scores_summary$avg_crps),
+      low = "white",
+      high = "darkred",
       guide = "colourbar", aesthetics = "fill"
     ) +
     geom_text(aes(
