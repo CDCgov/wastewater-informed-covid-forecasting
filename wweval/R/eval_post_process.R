acba37b39a68b6f73a4bc3714cbf2a5cc839a993
Dylan H. Morris <dzl1@cdc.gov>
2025-03-03 17:14:17 -0500

    Avoid R CMD check warning in temp function docs

--- wweval/R/eval_post_process.R
+++ wweval/R/eval_post_process.R
@@ -853,6 +853,15 @@ eval_post_process_hosp <- function(config_index,
 }
 
 #' placeholder wrapper for testing refactor
+#'
+#' @param config_index Index of eval_config to evaluate
+#' @param eval_config_path Path to eval_config (created with `write_eval_config`)
+#' @param params_path Path to params.toml
+#' @param model model to use. One of `"ww"` and `"hosp"`.
+#' @param max_eval_data_days Maximum number of days of data to pull
+#' when creating evaluation dataset. Default 365.
+#' @return NULL
+
 #' @export
 eval_postprocess <- function(config_index,
                              eval_config_path,
