ee1a633c50c7757ee63da02fe0fbe6bd8e799edd
Dylan H. Morris <dzl1@cdc.gov>
2025-03-03 15:47:14 -0500

    Rebase on main

--- wweval/R/eval_fit.R
+++ wweval/R/eval_fit.R
@@ -1,16 +1,19 @@
-#' Fit Wastewater Model for Evaluation
+#' Fit model for evaluation
 #'
 #' @param config_index Index of eval_config to evaluate
 #' @param eval_config_path Path to eval_config (created with `write_eval_config`)
 #' @param params_path Path to params.toml
-#'
+#' @param model Model to fit. One of `"ww"` and `"hosp"`
 #' @return NULL
 #' @export
 #'
+eval_fit <- function(config_index,
+                     eval_config_path,
+                     params_path,
+                     model) {
+  checkmate::assert_names(model, subset.of = c("ww", "hosp"))
+  ww_model <- model == "ww"
 
-eval_fit_ww <- function(config_index,
-                        eval_config_path,
-                        params_path) {
   eval_config <- yaml::read_yaml(eval_config_path)
   output_dir <- eval_config$output_dir
   raw_output_dir <- eval_config$raw_output_dir
@@ -161,13 +164,15 @@ eval_fit_hosp <- function(config_index,
   # Get the table of hospital admissions outliers ----------------------------
   table_of_exclusions <- tibble::as_tibble(eval_config$table_of_exclusions)
 
-  # Hospital admissions model fitting loop-----------------------------------------------
+
   raw_input_hosp_data <- get_input_hosp_data(
     forecast_date_i = forecast_date,
     location_i = location,
     hosp_data_dir = eval_config$hosp_data_dir,
     calibration_time = eval_config$calibration_time
   )
+
+
   input_hosp_data <- exclude_hosp_outliers(
     raw_input_hosp_data = raw_input_hosp_data,
     forecast_date = forecast_date,
@@ -179,27 +184,37 @@ eval_fit_hosp <- function(config_index,
 
   last_hosp_data_date <- get_last_hosp_data_date(input_hosp_data)
 
-  ## Use wwinference to fit the model------------------------------------------
-  hosp_fit_obj <- wwinference::wwinference(
-    ww_data = NULL,
-    count_data = input_hosp_data,
-    forecast_date = forecast_date,
-    calibration_time = eval_config$calibration_time,
-    forecast_horizon = eval_config$forecast_time,
-    model_spec = wwinference::get_model_spec(
-      generation_interval = eval_config$generation_interval,
-      inf_to_count_delay = wwinference::default_covid_inf_to_hosp, # eval_config$inf_to_hosp,
-      infection_feedback_pmf = eval_config$infection_feedback_pmf,
-      params = params,
-      include_ww = FALSE
-    ),
-    fit_opts = list(
-      seed = eval_config$seed,
-      iter_sampling = eval_config$iter_sampling,
-      adapt_delta = eval_config$adapt_delta,
-      chains = eval_config$n_chains,
-      max_treedepth = eval_config$max_treedepth
+  if (ww_model) {
+    ww_data_pull <- purrr::safely(get_input_ww_data)(forecast_date_i = forecast_date,
+      location_i = location,
+      scenario_i = scenario,
+      scenario_dir = eval_config$scenario_dir,
+      ww_data_dir = eval_config$ww_data_dir,
+      calibration_time = eval_config$calibration_time,
+      last_hosp_data_date = last_hosp_data_date,
+      ww_data_mapping = eval_config$ww_data_mapping
     )
+
+    input_ww_data <- ww_data_pull$result
+    save_object(input_ww_data)
+    include_ww <- TRUE
+  } else {
+    input_ww_data <- NULL
+    include_ww <- FALSE
+  }
+
+  model_spec <- wwinference::get_model_spec(
+    generation_interval = eval_config$generation_interval,
+    inf_to_count_delay = wwinference::default_covid_inf_to_hosp,
+    infection_feedback_pmf = eval_config$infection_feedback_pmf,
+    params = params
+  )
+  fit_opts <- list(
+    seed = eval_config$seed,
+    iter_sampling = eval_config$iter_sampling,
+    adapt_delta = eval_config$adapt_delta,
+    chains = eval_config$n_chains,
+    max_treedepth = eval_config$max_treedepth
   )
   save_object(hosp_fit_obj)
 }
