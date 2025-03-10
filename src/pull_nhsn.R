6bea57dc8d163e586e060ec81f9ab2f707dbce73
Dylan H. Morris <dzl1@cdc.gov>
2025-03-10 03:01:41 +0000

    Add location data to new eval pull

--- src/pull_nhsn.R
+++ src/pull_nhsn.R
@@ -1,8 +1,13 @@
 library(argparser)
-pull_and_write <- function(output_dir,
+pull_and_write <- function(location_data_path,
+                           output_dir,
                            force = FALSE) {
   pull_date <- lubridate::today()
 
+  location_data <- readr::read_csv(location_data_path) |>
+      dplyr::select(state = abbreviation,
+                    pop = population)
+  
   raw_data <- wweval::pull_nhsn(
     start_date = "2023-01-01",
     columns = c(
@@ -20,13 +25,16 @@ pull_and_write <- function(output_dir,
           .data$previous_day_admission_pediatric_covid_confirmed
         ),
       date = as.Date(.data$date) - lubridate::ddays(1)
-    ) |>
-    # convert from previous day to date-of-event indexing, following covidcast
+      ) |>
+      ## convert from previous day to date-of-event indexing,
+      ## following covidcast/epidatr
+    dplyr::inner_join(location_data, by = "state") |>
     dplyr::select(
       date,
-      ABBR = state, daily_hosp_admits
-    )
-
+      ABBR = state,
+      daily_hosp_admits,
+      pop)
+  
   output_path <- fs::path(output_dir, pull_date, ext = "csv")
 
   if (fs::file_exists(output_path) && !force) {
@@ -43,6 +51,10 @@ pull_and_write <- function(output_dir,
 
 p <- arg_parser("Pull NHSN data and save it to disk as a date-stamped csv.") |>
   add_argument(
+    "location_data_path",
+    help = "Path to a csv file containing location data."
+  ) |>
+  add_argument(
     "output_dir",
     help = "Directory in which to save the pulled data."
   ) |>
@@ -57,6 +69,7 @@ p <- arg_parser("Pull NHSN data and save it to disk as a date-stamped csv.") |>
 
 argv <- parse_args(p)
 pull_and_write(
+  location_data_path = argv$location_data_path,
   output_dir = argv$output_dir,
   force = argv$force
 )
