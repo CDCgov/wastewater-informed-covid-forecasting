51a39fe8c80d9064b226016d0b7d471134160a13
Dylan H. Morris <dzl1@cdc.gov>
2025-03-09 21:57:49 -0400

    Format files

--- src/pull_nhsn.R
+++ src/pull_nhsn.R
@@ -1,34 +1,43 @@
 library(argparser)
 pull_and_write <- function(output_dir,
-                 force = FALSE) {
-    pull_date <- lubridate::today()
+                           force = FALSE) {
+  pull_date <- lubridate::today()
 
-    raw_data <- wweval::pull_nhsn(
-                        start_date="2023-01-01",
-                        columns=c(
-                            "previous_day_admission_adult_covid_confirmed",
-                            "previous_day_admission_pediatric_covid_confirmed"))
+  raw_data <- wweval::pull_nhsn(
+    start_date = "2023-01-01",
+    columns = c(
+      "previous_day_admission_adult_covid_confirmed",
+      "previous_day_admission_pediatric_covid_confirmed"
+    )
+  )
 
-    data <- raw_data |>
-        dplyr::mutate(
-                   daily_hosp_admits =
-                       as.numeric(.data$previous_day_admission_adult_covid_confirmed) +
-                       as.numeric(.data$previous_day_admission_pediatric_covid_confirmed),
-                   date = as.Date(.data$date) - lubridate::ddays(1)) |>
-                                        # convert from previous day to date-of-event indexing, following covidcast
-        dplyr::select(
-                   date, ABBR=state, daily_hosp_admits)
+  data <- raw_data |>
+    dplyr::mutate(
+      daily_hosp_admits = as.numeric(
+        .data$previous_day_admission_adult_covid_confirmed
+      ) +
+        as.numeric(
+          .data$previous_day_admission_pediatric_covid_confirmed
+        ),
+      date = as.Date(.data$date) - lubridate::ddays(1)
+    ) |>
+    # convert from previous day to date-of-event indexing, following covidcast
+    dplyr::select(
+      date,
+      ABBR = state, daily_hosp_admits
+    )
 
-    output_path <- fs::path(output_dir, pull_date, ext = "csv")
+  output_path <- fs::path(output_dir, pull_date, ext = "csv")
 
-    if(fs::file_exists(output_path) && !force) {
-        cli::cli_abort(c(
-                 "File {output_path} already exists. Run with force = TRUE",
-                 "to overwrite it"))
-    } else {
-        cli::cli_inform("Saving output to {output_path}...")
-        readr::write_csv(data, output_path)
-    }
+  if (fs::file_exists(output_path) && !force) {
+    cli::cli_abort(c(
+      "File {output_path} already exists. Run with force = TRUE",
+      "to overwrite it"
+    ))
+  } else {
+    cli::cli_inform("Saving output to {output_path}...")
+    readr::write_csv(data, output_path)
+  }
 }
 
 
@@ -40,11 +49,14 @@ p <- arg_parser("Pull NHSN data and save it to disk as a date-stamped csv.") |>
   add_argument(
     "--force",
     help = paste0(
-        "If provided, overwrite existing data with the same datestamp, if any"),
+      "If provided, overwrite existing data with the same datestamp, if any"
+    ),
     flag = TRUE
-)
+  )
 
 
 argv <- parse_args(p)
-pull_and_write(output_dir = argv$output_dir,
-               force = argv$force)
+pull_and_write(
+  output_dir = argv$output_dir,
+  force = argv$force
+)
