1781bfdd9052d0a23d33aa504f1e34dc306e7da6
Aisha Siddiqui <ASiddiqui@cdc.gov>
2025-04-09 15:22:34 -0400

    fix cached layer tags

--- .github/scripts/docker_build_and_push.sh
+++ .github/scripts/docker_build_and_push.sh
@@ -8,7 +8,7 @@ docker buildx create --name "$BUILDER" --driver=docker-container || true
 # use the registry cache for prior images of the same tag, or the 'latest' tag
 time docker buildx build --push -t "$IMAGE:$TAG" \
 	--builder "$BUILDER" \
-	--cache-from "type=registry,ref=$IMAGE:$TAG-cache" \
-	--cache-from "type=registry,ref=$IMAGE:latest-cache" \
-	--cache-to "type=registry,ref=$IMAGE:$TAG-cache,mode=max" \
+	--cache-from "type=registry,ref=$IMAGE:$TAG" \
+	--cache-from "type=registry,ref=$IMAGE:latest" \
+	--cache-to "type=registry,ref=$IMAGE:$TAG,mode=max" \
 	-f Containerfile .
