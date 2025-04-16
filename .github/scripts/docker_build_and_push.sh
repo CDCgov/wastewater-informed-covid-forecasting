c45591e1361daa33ee179e82363229dfb4bd00db
Dylan H. Morris <dylanhmorris@users.noreply.github.com>
2025-04-16 17:12:16 -0400

    Update docker_build_and_push.sh

--- .github/scripts/docker_build_and_push.sh
+++ .github/scripts/docker_build_and_push.sh
@@ -1,5 +1,7 @@
 IMAGE="ghcr.io/cdcgov/$1"
 TAG=$2
+GIT_COMMIT_SHA=$3
+GIT_BRANCH_NAME=$4
 BUILDER=docker-container-driver-builder
 
 # create a builder with the docker-container driver to allow cache-export
@@ -8,7 +10,9 @@ docker buildx create --name "$BUILDER" --driver=docker-container || true
 # use the registry cache for prior images of the same tag, or the 'latest' tag
 time docker buildx build --push -t "$IMAGE:$TAG" \
 	--builder "$BUILDER" \
-	--cache-from "type=registry,ref=$IMAGE:$TAG" \
-	--cache-from "type=registry,ref=$IMAGE:latest" \
-	--cache-to "type=registry,ref=$IMAGE:$TAG,mode=max" \
+	--build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
+	--build-arg "GIT_BRANCH_NAME=$GIT_BRANCH_NAME" \
+	--cache-from "type=registry,ref=$IMAGE:$TAG-cache" \
+	--cache-from "type=registry,ref=$IMAGE:latest-cache" \
+	--cache-to "type=registry,ref=$IMAGE:$TAG-cache,mode=max" \
 	-f Containerfile .
