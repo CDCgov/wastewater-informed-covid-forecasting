122b1993288eb0292dfe470e7d8fd31cdafbe82c
Dylan H. Morris <dzl1@cdc.gov>
2025-05-30 12:23:53 -0400

    More linting

--- wweval/R/get_table_sufficient_ww.R
+++ wweval/R/get_table_sufficient_ww.R
@@ -17,7 +17,9 @@
 get_table_sufficient_ww <- function(combined_ww_data_flags) {
   # Ensure all `values` are boolean
   stopifnot(
-    "In diagnostic table checking for sufficent wastewater data flags, not all values are boolean" = is.logical(
+      paste0("In diagnostic table checking for sufficent ",
+             "wastewater data flags, not all values are ",
+             "boolean") = is.logical(
       combined_ww_data_flags$value
     )
   )
