ce9823648043f03d3e1ff5d793d9cf427af82bad
Dylan H. Morris <dzl1@cdc.gov>
2025-05-30 13:36:51 -0400

    Fix one more exclusion

--- wweval/R/plot_calibration.R
+++ wweval/R/plot_calibration.R
@@ -8,17 +8,17 @@
 #' each interval for each model.
 #' @export
 forecast_qq_plot <- function(forecasts, time_period) {
-        colors <- plot_components()
-        p <- scoringutils::get_coverage(forecasts) |>
-                scoringutils::plot_quantile_coverage() +
-                scale_y_continuous(
-                        labels = scales::label_percent()
-                ) +
-                ggtitle(glue::glue("QQ plot for {time_period}")) +
-                get_plot_theme() +
-                scale_color_manual(values = colors$model_colors) +
-                coord_fixed()
-        return(p)
+  colors <- plot_components()
+  p <- scoringutils::get_coverage(forecasts) |>
+    scoringutils::plot_quantile_coverage() +
+    scale_y_continuous(
+      labels = scales::label_percent()
+    ) +
+    ggtitle(glue::glue("QQ plot for {time_period}")) +
+    get_plot_theme() +
+    scale_color_manual(values = colors$model_colors) +
+    coord_fixed()
+  return(p)
 }
 
 #' Plot interval coverage at specified ranges
@@ -34,51 +34,51 @@ forecast_qq_plot <- function(forecasts, time_period) {
 #' @export
 # nolint start
 forecast_interval_coverage_plot <- function(
-        # nolint end
-        forecasts,
-        ranges,
-        time_period,
-        by = c("model", "horizon")
+  # nolint end
+  forecasts,
+  ranges,
+  time_period,
+  by = c("model", "horizon")
 ) {
-        to_plot <- scoringutils::get_coverage(
-                forecasts,
-                by = by
-        ) |>
-                dplyr::mutate(
-                        named_facet = glue::glue("{.data$interval_range}%")
-                ) |>
-                order_horizons() |>
-                dplyr::filter(.data$interval_range %in% !!ranges)
-        colors <- plot_components()
-        p <- ggplot(
-                data = to_plot,
-                mapping = aes(
-                        x = .data$horizon,
-                        y = .data$interval_coverage,
-                        color = .data$model
-                )
-        ) +
-                geom_line(aes(group = .data$model), linetype = "dashed") +
-                geom_point() +
-                geom_hline(
-                        aes(yintercept = .data$interval_range / 100),
-                        linetype = "dashed"
-                ) +
-                facet_wrap(~ .data$named_facet, scales = "free_y") +
-                labs(
-                        y = "Proportion of data within interval",
-                        x = "Forecast horizon",
-                        col = "Model"
-                ) +
-                scale_y_continuous(
-                        expand = expansion(c(0.2, 0.2)),
-                        labels = scales::label_percent()
-                ) +
-                scale_x_discrete() +
-                get_plot_theme(
-                        x_axis_dates = TRUE
-                ) +
-                scale_color_manual(values = colors$model_colors)
+  to_plot <- scoringutils::get_coverage(
+    forecasts,
+    by = by
+  ) |>
+    dplyr::mutate(
+      named_facet = glue::glue("{.data$interval_range}%")
+    ) |>
+    order_horizons() |>
+    dplyr::filter(.data$interval_range %in% !!ranges)
+  colors <- plot_components()
+  p <- ggplot(
+    data = to_plot,
+    mapping = aes(
+      x = .data$horizon,
+      y = .data$interval_coverage,
+      color = .data$model
+    )
+  ) +
+    geom_line(aes(group = .data$model), linetype = "dashed") +
+    geom_point() +
+    geom_hline(
+      aes(yintercept = .data$interval_range / 100),
+      linetype = "dashed"
+    ) +
+    facet_wrap(~ .data$named_facet, scales = "free_y") +
+    labs(
+      y = "Proportion of data within interval",
+      x = "Forecast horizon",
+      col = "Model"
+    ) +
+    scale_y_continuous(
+      expand = expansion(c(0.2, 0.2)),
+      labels = scales::label_percent()
+    ) +
+    scale_x_discrete() +
+    get_plot_theme(
+      x_axis_dates = TRUE
+    ) +
+    scale_color_manual(values = colors$model_colors)
 
-        return(p)
+  return(p)
 }
