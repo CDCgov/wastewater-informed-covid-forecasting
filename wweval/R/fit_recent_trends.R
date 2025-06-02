2b5e5eca74c933c9c312d2490853ddda160bf209
Dylan H. Morris <dzl1@cdc.gov>
2025-06-02 21:23:46 +0000

    Explicit control param

--- wweval/R/fit_recent_trends.R
+++ wweval/R/fit_recent_trends.R
@@ -19,117 +19,120 @@
 #' @param ww_lookback_days Number of days to look back when fitting the
 #' trend in wastewater concentrations.
 #' @param seed Seed for Stan's pseudorandom number generator.
+#' @param control list of control parameters passed to stan via
+#' [brms::brm()]. Default `NULL`.
 #' @param ... Additional keyword arguments passed to [brms::brm()]
 #' via [fit_hosp_trend()].
 #' @return The two fit objects, as a list, saving raw fits to disk
 #' as a side effect.
 #' @export
 fit_recent_trends <- function(
-  forecast_date,
-  location,
-  scenario,
-  raw_output_dir,
-  hosp_prior_params,
-  ww_prior_params,
-  hosp_lookback_days,
-  ww_lookback_days,
-  seed,
-  ...
+        forecast_date,
+        location,
+        scenario,
+        raw_output_dir,
+        hosp_prior_params,
+        ww_prior_params,
+        hosp_lookback_days,
+        ww_lookback_days,
+        seed,
+        control,
+        ...
 ) {
-  general_fit_params <- c("data", "seed", "...")
+        general_fit_params <- c("data", "seed", "control", "...")
 
-  needed_hosp_prior_params <- setdiff(
-    names(formals(fit_hosp_trend)),
-    general_fit_params
-  )
-  needed_ww_prior_params <- setdiff(
-    names(formals(fit_ww_trend)),
-    general_fit_params
-  )
+        needed_hosp_prior_params <- setdiff(
+                names(formals(fit_hosp_trend)),
+                general_fit_params
+        )
+        needed_ww_prior_params <- setdiff(
+                names(formals(fit_ww_trend)),
+                general_fit_params
+        )
 
-  checkmate::assert_names(
-    names(hosp_prior_params),
-    permutation.of = needed_hosp_prior_params
-  )
-  checkmate::assert_names(
-    names(ww_prior_params),
-    permutation.of = needed_ww_prior_params
-  )
+        checkmate::assert_names(
+                names(hosp_prior_params),
+                permutation.of = needed_hosp_prior_params
+        )
+        checkmate::assert_names(
+                names(ww_prior_params),
+                permutation.of = needed_ww_prior_params
+        )
 
-  load_object <- get_object_loader(
-    location,
-    forecast_date,
-    scenario,
-    raw_output_dir
-  )
-  save_object <- get_object_saver(
-    location,
-    forecast_date,
-    scenario,
-    raw_output_dir
-  )
+        load_object <- get_object_loader(
+                location,
+                forecast_date,
+                scenario,
+                raw_output_dir
+        )
+        save_object <- get_object_saver(
+                location,
+                forecast_date,
+                scenario,
+                raw_output_dir
+        )
 
-  first_hosp_trend_date <-
-    as.Date(forecast_date) - lubridate::days(hosp_lookback_days)
-  first_ww_trend_date <-
-    as.Date(forecast_date) - lubridate::days(ww_lookback_days)
-  message("Loading input data...")
+        first_hosp_trend_date <-
+                as.Date(forecast_date) - lubridate::days(hosp_lookback_days)
+        first_ww_trend_date <-
+                as.Date(forecast_date) - lubridate::days(ww_lookback_days)
+        message("Loading input data...")
 
-  input_hosp_data <- load_object("input_hosp_data") |>
-    dplyr::transmute(
-      time = as.numeric(
-        .data$date - !!first_hosp_trend_date
-      ) +
-        1L,
-      hosp = .data$count
-    ) |>
-    dplyr::filter(.data$time > 0)
+        input_hosp_data <- load_object("input_hosp_data") |>
+                dplyr::transmute(
+                        time = as.numeric(
+                                .data$date - !!first_hosp_trend_date
+                        ) +
+                                1L,
+                        hosp = .data$count
+                ) |>
+                dplyr::filter(.data$time > 0)
 
-  input_ww_data <- load_object("input_ww_data") |>
-    dplyr::mutate(
-      time = as.numeric(
-        .data$date - !!first_ww_trend_date
-      ) +
-        1L,
-      conc = ifelse(
-        .data$below_lod,
-        .data$log_lod,
-        .data$log_genome_copies_per_ml
-      ),
-      cens = ifelse(.data$below_lod, -1, 0)
-    ) |>
-    dplyr::filter(.data$time > 0) |>
-    dplyr::select(
-      "time",
-      "lab_site_index",
-      "conc",
-      "cens"
-    )
+        input_ww_data <- load_object("input_ww_data") |>
+                dplyr::mutate(
+                        time = as.numeric(
+                                .data$date - !!first_ww_trend_date
+                        ) +
+                                1L,
+                        conc = ifelse(
+                                .data$below_lod,
+                                .data$log_lod,
+                                .data$log_genome_copies_per_ml
+                        ),
+                        cens = ifelse(.data$below_lod, -1, 0)
+                ) |>
+                dplyr::filter(.data$time > 0) |>
+                dplyr::select(
+                        "time",
+                        "lab_site_index",
+                        "conc",
+                        "cens"
+                )
 
-  message("Fitting admissions trend...")
-  hosp_args <- c(
-    list(data = input_hosp_data),
-    hosp_prior_params,
-    list(seed = seed),
-    ...
-  )
-  recent_hosp_trend_fit <- do.call(fit_hosp_trend, hosp_args)
-  save_object(recent_hosp_trend_fit)
+        message("Fitting admissions trend...")
+        hosp_args <- c(
+                list(data = input_hosp_data),
+                hosp_prior_params,
+                list(seed = seed, control = control),
+                ...
+        )
+        recent_hosp_trend_fit <- do.call(fit_hosp_trend, hosp_args)
+        save_object(recent_hosp_trend_fit)
 
-  message("Fitting  wastewater trend...")
-  ww_args <- c(
-    list(data = input_ww_data),
-    ww_prior_params,
-    list(seed = seed),
-    ...
-  )
-  recent_ww_trend_fit <- do.call(fit_ww_trend, ww_args)
-  save_object(recent_ww_trend_fit)
+        message("Fitting  wastewater trend...")
+        ww_args <- c(
+                list(data = input_ww_data),
+                ww_prior_params,
+                list(seed = seed, control = control),
+                ...
+        )
+        recent_ww_trend_fit <- do.call(fit_ww_trend, ww_args)
+        save_object(recent_ww_trend_fit)
 
-  return(list(
-    hosp = recent_hosp_trend_fit,
-    ww = recent_ww_trend_fit
-  ))
+        return(list(
+                hosp = recent_hosp_trend_fit,
+                ww = recent_ww_trend_fit
+        ))
 }
 
 
@@ -145,53 +148,57 @@ fit_recent_trends <- function(
 #' @param log_nb_conc_prior_sd Standard deviation for the Normal prior on
 #' the log of the negative binomial concentration parameter ("phi").
 #' @param seed Seed for cmdnstan's pseudorandom number generator.
+#' @param control list of control parameters passed to stan via
+#' [brms::brm()]. Default `NULL`.
 #' @param ... Additional keyword arguments passed to [brms::brm()]
 #' @export
 fit_hosp_trend <- function(
-  data,
-  exp_rate_hosp_prior_mean,
-  exp_rate_hosp_prior_sd,
-  log_nb_conc_prior_mean,
-  log_nb_conc_prior_sd,
-  seed,
-  ...
+        data,
+        exp_rate_hosp_prior_mean,
+        exp_rate_hosp_prior_sd,
+        log_nb_conc_prior_mean,
+        log_nb_conc_prior_sd,
+        seed,
+        control,
+        ...
 ) {
-  checkmate::assert_names(
-    names(data),
-    must.include = c("hosp", "time")
-  )
-  fit <- brms::brm(
-    formula = brms::bf(hosp ~ time),
-    data = data,
-    family = brms::negbinomial(
-      link = "log",
-      link_shape = "log"
-    ),
-    prior = c(
-      brms::prior_string(
-        glue::glue(
-          "normal(",
-          "{exp_rate_hosp_prior_mean}, ",
-          "{exp_rate_hosp_prior_sd})"
-        ),
-        class = "b",
-        coef = "time"
-      ),
-      brms::prior_string(
-        glue::glue(
-          "normal(",
-          "{log_nb_conc_prior_mean}, ",
-          "{log_nb_conc_prior_sd})"
-        ),
-        class = "shape"
-      )
-    ),
-    seed = seed,
-    backend = "cmdstanr",
-    ...
-  )
+        checkmate::assert_names(
+                names(data),
+                must.include = c("hosp", "time")
+        )
+        fit <- brms::brm(
+                formula = brms::bf(hosp ~ time),
+                data = data,
+                family = brms::negbinomial(
+                        link = "log",
+                        link_shape = "log"
+                ),
+                prior = c(
+                        brms::prior_string(
+                                glue::glue(
+                                        "normal(",
+                                        "{exp_rate_hosp_prior_mean}, ",
+                                        "{exp_rate_hosp_prior_sd})"
+                                ),
+                                class = "b",
+                                coef = "time"
+                        ),
+                        brms::prior_string(
+                                glue::glue(
+                                        "normal(",
+                                        "{log_nb_conc_prior_mean}, ",
+                                        "{log_nb_conc_prior_sd})"
+                                ),
+                                class = "shape"
+                        )
+                ),
+                seed = seed,
+                backend = "cmdstanr",
+                control = control,
+                ...
+        )
 
-  return(fit)
+        return(fit)
 }
 
 
@@ -244,109 +251,113 @@ fit_hosp_trend <- function(
 #' observation error standard deviations (which governs the variability
 #' in observation error scale among lab-sites).
 #' @param seed Seed for cmdnstan's pseudorandom number generator.
+#' @param control list of control parameters passed to stan via
+#' [brms::brm()]. Default `NULL`.
 #' @param ... Additional keyword arguments passed to [brms::brm()]
 #' @export
 fit_ww_trend <- function(
-  data,
-  exp_rate_conc_pop_prior_mean,
-  exp_rate_conc_pop_prior_sd,
-  sd_exp_rate_conc_prior_mode,
-  sd_exp_rate_conc_prior_sd,
-  t0_conc_pop_prior_mean,
-  t0_conc_pop_prior_sd,
-  sd_t0_conc_prior_mode,
-  sd_t0_conc_prior_sd,
-  log_sd_obs_pop_prior_mean,
-  log_sd_obs_pop_prior_sd,
-  sd_log_sd_obs_prior_mode,
-  sd_log_sd_obs_prior_sd,
-  seed,
-  ...
+        data,
+        exp_rate_conc_pop_prior_mean,
+        exp_rate_conc_pop_prior_sd,
+        sd_exp_rate_conc_prior_mode,
+        sd_exp_rate_conc_prior_sd,
+        t0_conc_pop_prior_mean,
+        t0_conc_pop_prior_sd,
+        sd_t0_conc_prior_mode,
+        sd_t0_conc_prior_sd,
+        log_sd_obs_pop_prior_mean,
+        log_sd_obs_pop_prior_sd,
+        sd_log_sd_obs_prior_mode,
+        sd_log_sd_obs_prior_sd,
+        seed,
+        control,
+        ...
 ) {
-  checkmate::assert_names(
-    names(data),
-    must.include = c(
-      "time",
-      "lab_site_index",
-      "conc",
-      "cens"
-    )
-  )
+        checkmate::assert_names(
+                names(data),
+                must.include = c(
+                        "time",
+                        "lab_site_index",
+                        "conc",
+                        "cens"
+                )
+        )
 
-  ww_formula <- brms::bf(
-    conc | cens(cens) ~ time + (time || lab_site_index),
-    sigma ~ (1 || lab_site_index)
-  )
-  ww_priors <- c(
-    brms::prior_string(
-      glue::glue(
-        "normal(",
-        "{exp_rate_conc_pop_prior_mean}, ",
-        "{exp_rate_conc_pop_prior_sd})"
-      ),
-      class = "b",
-      coef = "time"
-    ),
-    brms::prior_string(
-      glue::glue(
-        "normal(",
-        "{sd_exp_rate_conc_prior_mode}, ",
-        "{sd_exp_rate_conc_prior_sd})"
-      ),
-      class = "sd",
-      coef = "time",
-      group = "lab_site_index"
-    ),
-    brms::prior_string(
-      glue::glue(
-        "normal(",
-        "{t0_conc_pop_prior_mean}, ",
-        "{t0_conc_pop_prior_sd})"
-      ),
-      class = "Intercept"
-    ),
-    brms::prior_string(
-      glue::glue(
-        "normal({sd_t0_conc_prior_mode}, ",
-        "{sd_t0_conc_prior_sd})"
-      ),
-      class = "sd",
-      coef = "Intercept",
-      group = "lab_site_index"
-    ),
-    brms::prior_string(
-      glue::glue(
-        "normal(",
-        "{log_sd_obs_pop_prior_mean}, ",
-        "{log_sd_obs_pop_prior_sd})"
-      ),
-      dpar = "sigma",
-      class = "Intercept"
-    ),
-    brms::prior_string(
-      glue::glue(
-        "normal(",
-        "{sd_log_sd_obs_prior_mode}, ",
-        "{sd_log_sd_obs_prior_sd})"
-      ),
-      dpar = "sigma",
-      class = "sd",
-      group = "lab_site_index"
-    )
-  )
-  fit <- brms::brm(
-    formula = ww_formula,
-    data = data,
-    family = brms::brmsfamily(
-      "gaussian",
-      link = "identity",
-      link_sigma = "log"
-    ),
-    prior = ww_priors,
-    seed = seed,
-    backend = "cmdstanr",
-    ...
-  )
+        ww_formula <- brms::bf(
+                conc | cens(cens) ~ time + (time || lab_site_index),
+                sigma ~ (1 || lab_site_index)
+        )
+        ww_priors <- c(
+                brms::prior_string(
+                        glue::glue(
+                                "normal(",
+                                "{exp_rate_conc_pop_prior_mean}, ",
+                                "{exp_rate_conc_pop_prior_sd})"
+                        ),
+                        class = "b",
+                        coef = "time"
+                ),
+                brms::prior_string(
+                        glue::glue(
+                                "normal(",
+                                "{sd_exp_rate_conc_prior_mode}, ",
+                                "{sd_exp_rate_conc_prior_sd})"
+                        ),
+                        class = "sd",
+                        coef = "time",
+                        group = "lab_site_index"
+                ),
+                brms::prior_string(
+                        glue::glue(
+                                "normal(",
+                                "{t0_conc_pop_prior_mean}, ",
+                                "{t0_conc_pop_prior_sd})"
+                        ),
+                        class = "Intercept"
+                ),
+                brms::prior_string(
+                        glue::glue(
+                                "normal({sd_t0_conc_prior_mode}, ",
+                                "{sd_t0_conc_prior_sd})"
+                        ),
+                        class = "sd",
+                        coef = "Intercept",
+                        group = "lab_site_index"
+                ),
+                brms::prior_string(
+                        glue::glue(
+                                "normal(",
+                                "{log_sd_obs_pop_prior_mean}, ",
+                                "{log_sd_obs_pop_prior_sd})"
+                        ),
+                        dpar = "sigma",
+                        class = "Intercept"
+                ),
+                brms::prior_string(
+                        glue::glue(
+                                "normal(",
+                                "{sd_log_sd_obs_prior_mode}, ",
+                                "{sd_log_sd_obs_prior_sd})"
+                        ),
+                        dpar = "sigma",
+                        class = "sd",
+                        group = "lab_site_index"
+                )
+        )
+        fit <- brms::brm(
+                formula = ww_formula,
+                data = data,
+                family = brms::brmsfamily(
+                        "gaussian",
+                        link = "identity",
+                        link_sigma = "log"
+                ),
+                prior = ww_priors,
+                seed = seed,
+                backend = "cmdstanr",
+                control = control,
+                ...
+        )
 
-  return(fit)
+        return(fit)
 }
