4eaa424d45695ae0e321ed7ed39656f61c7e98c1
Dylan H. Morris <dzl1@cdc.gov>
2025-06-02 21:23:58 +0000

    Explicit control param

--- wweval/R/process_recent_trend_fits.R
+++ wweval/R/process_recent_trend_fits.R
@@ -9,51 +9,51 @@
 #' disk as a side effect.
 #' @export
 process_recent_trend_fits <- function(
-        hosp_fit,
-        ww_fit,
-        figure_save_dir,
-        figure_ext = "pdf"
+  hosp_fit,
+  ww_fit,
+  figure_save_dir,
+  figure_ext = "pdf"
 ) {
-        conditions <- dplyr::distinct(
-                ww_fit$data,
-                .data$lab_site_index
-        ) |>
-                head(10)
-        ww_plot <- brms::conditional_effects(
-                ww_fit,
-                "time",
-                re_formula = NULL,
-                conditions = conditions
-        ) |>
-                plot(points = TRUE)
+  conditions <- dplyr::distinct(
+    ww_fit$data,
+    .data$lab_site_index
+  ) |>
+    head(10)
+  ww_plot <- brms::conditional_effects(
+    ww_fit,
+    "time",
+    re_formula = NULL,
+    conditions = conditions
+  ) |>
+    plot(points = TRUE)
 
-        ww_plot <- ww_plot$time + theme_minimal()
+  ww_plot <- ww_plot$time + theme_minimal()
 
-        hosp_plot <- brms::conditional_effects(
-                hosp_fit,
-                "time",
-        ) |>
-                plot(points = TRUE)
-        hosp_plot <- hosp_plot$time +
-                scale_y_continuous(transform = "log10") +
-                theme_minimal()
+  hosp_plot <- brms::conditional_effects(
+    hosp_fit,
+    "time",
+  ) |>
+    plot(points = TRUE)
+  hosp_plot <- hosp_plot$time +
+    scale_y_continuous(transform = "log10") +
+    theme_minimal()
 
-        ggsave(
-                filename = fs::path(
-                        figure_save_dir,
-                        "ww_recent_trend_plot",
-                        ext = figure_ext
-                ),
-                plot = ww_plot
-        )
-        ggsave(
-                filename = fs::path(
-                        figure_save_dir,
-                        "hosp_recent_trend_plot",
-                        ext = figure_ext
-                ),
-                plot = hosp_plot
-        )
+  ggsave(
+    filename = fs::path(
+      figure_save_dir,
+      "ww_recent_trend_plot",
+      ext = figure_ext
+    ),
+    plot = ww_plot
+  )
+  ggsave(
+    filename = fs::path(
+      figure_save_dir,
+      "hosp_recent_trend_plot",
+      ext = figure_ext
+    ),
+    plot = hosp_plot
+  )
 
-        invisible()
+  invisible()
 }
