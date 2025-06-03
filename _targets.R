ca21ee677d2c4c060906e3c0ad71b72cbfb609fc
Dylan H. Morris <dzl1@cdc.gov>
2025-06-03 18:18:06 -0400

    Format

--- _targets.R
+++ _targets.R
@@ -1109,8 +1109,7 @@ real_time_rel_targets <- list(
       dplyr::anti_join(ww_forecast_date_locs_to_excl) |>
       dplyr::left_join(table_of_loc_dates_w_ww) |>
       dplyr::filter(.data$ww_sufficient) |>
-        dplyr::select(-c("ww_sufficient",
-                         "failed_convergence")) |>
+      dplyr::select(-c("ww_sufficient", "failed_convergence")) |>
       scoringutils:::as_scores(
         metrics = names(
           wweval::quantile_metrics
