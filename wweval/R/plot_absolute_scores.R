83da880cc21a7a2058d7a66ecc41762347a6e74b
Dylan H. Morris <dzl1@cdc.gov>
2025-06-16 17:04:04 -0400

    Label support in scatters

--- wweval/R/plot_absolute_scores.R
+++ wweval/R/plot_absolute_scores.R
@@ -134,9 +134,10 @@ wis_barplot <- function(scores) {
 #' @param by Summize scores by these columns. Passed as the
 #' `by` argument to [forecasttools::summarise_scores_with_baseline()].
 #' Default `NULL`.
-#' @param label Label the points according to the columns in `by`?
-#' Default `FALSE`.
-#' @param ... keyword arguments passed to [ggplot2::geom_point()].
+#' @param label_col If provided, plot the value of this column
+#' via [geom_text()] in lieu of points. Default `NULL` (plot points).
+#' @param ... keyword arguments passed to [ggplot2::geom_point()]
+#' or [ggplot2::geom_text()], depending on the value of label_col.
 #' @return The scatterplot, as ggplot object
 #' @export
 plot_score_scatter <- function(
@@ -145,7 +146,7 @@ plot_score_scatter <- function(
   model_x,
   model_y,
   by = NULL,
-  label = FALSE,
+  label_col = NULL,
   ...
 ) {
   to_plot <- scores |>
@@ -160,17 +161,25 @@ plot_score_scatter <- function(
     dplyr::filter(.data$model == !!model_y) |>
     dplyr::mutate(
       score_y = .data[[metric]],
-      score_x = .data[[metric]] / .data$mean_scores_ratio,
-      label = ifelse(label, interaction(dplyr::across(data, by)), "")
+      score_x = .data[[metric]] / .data$mean_scores_ratio
     )
 
   all_vals <- c(to_plot$score_x, to_plot$score_y)
   minval <- min(all_vals)
   maxval <- max(all_vals)
 
+  if (!is.null(label_col)) {
+    geom_to_plot <- geom_text(
+      mapping = aes(label = .data[[label_col]]),
+      ...
+    )
+  } else {
+    geom_to_plot <- geom_point(...)
+  }
+
   p <- ggplot(
     data = to_plot,
-    mapping = aes(x = .data$score_x, y = .data$score_y, label = .data$label)
+    mapping = aes(x = .data$score_x, y = .data$score_y)
   ) +
     geom_abline(
       slope = 1,
@@ -178,7 +187,7 @@ plot_score_scatter <- function(
       linetype = "dashed",
       linewidth = 2
     ) +
-    geom_point(...) +
+    geom_to_plot +
     get_plot_theme() +
     coord_fixed(xlim = c(minval, maxval), ylim = c(minval, maxval)) +
     labs(
