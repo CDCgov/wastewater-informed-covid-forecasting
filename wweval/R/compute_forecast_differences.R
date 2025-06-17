eb2ad104c43fdc1a417a460abf973a68df621382
Dylan H. Morris <dzl1@cdc.gov>
2025-06-16 23:48:39 -0400

    Add skeleton for computing diffs

---
+++ wweval/R/compute_forecast_differences.R
@@ -0,0 +1,43 @@
+#' Compute the the posterior of the difference between model predictions.
+#'
+#' @param forecast_date forecast_date for which to fits recent data
+#' trends. Data for an actual forecast must already have been produced
+#' and exist in `raw_output_dir`.
+#' @param location Location for which to fit trends.
+#' @param scenario Wastewater data availability scenario to analyze.
+#' @param raw_output_dir Directory containing raw output `.rds` files.
+#' Used to obtain the admissions and wastewater data used in fitting
+#' the forecasting model.
+#' @return The posterior of the difference, as a table..
+#' @export
+compute_foreacst_difference <- function(
+  forecast_date,
+  location,
+  scenario,
+  raw_output_dir
+) {
+  load_object_hosp <- get_object_loader(
+    location,
+    forecast_date,
+    "no_wastewater",
+    raw_output_dir
+  )
+  load_object_ww <- get_object_loader(
+    location,
+    forecast_date,
+    scenario,
+    raw_output_dir
+  )
+
+  save_object <- get_object_saver(
+    location,
+    forecast_date,
+    scenario,
+    raw_output_dir
+  )
+
+  message("Loading posteriors...")
+
+  preds_hosp <- load_object_hosp("hosp_draws")
+  preds_ww <- load_object_ww("hosp_draws")
+}
