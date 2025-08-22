96ea0bf70b5942898acee4caf93ab436fadb51ca
Dylan H. Morris <dzl1@cdc.gov>
2025-08-22 14:51:16 -0400

    Pull in change to postprocess

--- wweval/R/postprocess.R
+++ wweval/R/postprocess.R
@@ -642,7 +642,7 @@ postprocess_successful_fit <- function(
   hosp_scores_quantiles <- score_quantiles(
     hosp_quantiles,
     scenario,
-    metrics = metrics_quantiles
+    metrics = quantile_metrics
   )
   save_object(hosp_scores_quantiles)
   save_table(
