8e813faaab83c6707fd2edf225ea3b9feb3b5e3f
Dylan H. Morris <dzl1@cdc.gov>
2025-08-25 19:33:33 +0000

    Fix CRPS by horizon plot

--- wweval/R/plot_absolute_scores.R
+++ wweval/R/plot_absolute_scores.R
@@ -279,3 +279,55 @@ heatmap_scores_by_loc_date <- function(scores, metric, models_to_plot) {
 
   return(p)
 }
+
+
+#' Get plot of scores by horizon over time
+#'
+#' @param scores tibble of scores for every location, forecast date, and horizon
+#' @param score_type Score to display, as its column name in `scores`.
+#' @param score_display_name Display name for the score. If not specified,
+#' use the value of `score_type` in uppercase.
+#' @return plot of scores over time faceted by horizon
+#' @export
+get_plot_score_by_horizon_t <- function(scores,
+                                        score_type,
+                                        score_display_name = toupper(score_type)) {
+  scores_by_horizon_and_t <- scoringutils::summarise_scores(
+    scores,
+    by = c("model", "forecast_date", "horizon")
+    ) |>
+      dplyr::mutate(
+                 horizon = factor(.data$horizon,
+                                  ordered = TRUE,
+                                  levels = unique(c(
+                                      "nowcast", # put nowcast first
+                                      sort(.data$horizon)))))
+  p <- ggplot(scores_by_horizon_and_t,
+              mapping = aes(
+                  x = .data$forecast_date,
+                  y = .data[[score_type]],
+                  color = .data$model
+              )) +
+      forecasttools::geom_line_point(size = 2,
+                                     linewidth = 1.5,
+                                     alpha = 0.5) +
+    get_plot_theme(
+      x_axis_dates = TRUE,
+      y_axis_title_size = 8
+    ) +
+    facet_wrap(~ .data$horizon, ncol = 1) +
+    scale_x_date(
+      date_breaks = "2 weeks",
+      date_labels = "%Y-%m-%d"
+    ) +
+    scale_color_model() +
+    xlab(NULL) +
+    ylab(glue::glue("Average {score_display_name}")) +
+    ggtitle(glue::glue(
+      "Average {score_display_name} over time, across locations"
+      )) +
+      theme(legend.position = "bottom") +
+      guides(color = guide_legend(nrow = 2))
+
+  return(p)
+}
