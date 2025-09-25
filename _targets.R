cb88ea19e4d6211db4aaeee84364446c0672dc01
Dylan H. Morris <dzl1@cdc.gov>
2025-09-25 10:25:43 -0400

    Document and format

--- _targets.R
+++ _targets.R
@@ -455,9 +455,8 @@ collated_output_targets <- list(
   ),
   tar_target(
     name = ww_sufficiency_table, # backward compatibility
-    command = ww_quality_table |> dplyr::select("forecast_date",
-                                                "location",
-                                                "ww_sufficient")
+    command = ww_quality_table |>
+      dplyr::select("forecast_date", "location", "ww_sufficient")
   ),
   tar_target(
     name = date_locs_sufficient_ww,
