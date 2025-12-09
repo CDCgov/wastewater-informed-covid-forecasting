767d1f1437e2f9b0626c76950c8cfe59ee3fec27
Dylan H. Morris <dzl1@cdc.gov>
2025-12-08 21:53:46 -0500

    Qualify namespace

--- _targets.R
+++ _targets.R
@@ -1369,15 +1369,14 @@ trend_analysis_targets <- list(
       )
   ),
   tar_target(
-    name = trend_diff_metric_names,
-    command = c(
-      "global_slope_hosp" = "Admissions trend (global slope)",
-      "global_slope_ww" = "Wastewater trend (global slope)",
-      "sd_slope_ww" = "Wastewater trend variability (slope s.d.)",
-      "diff_slope_ww_hosp" = "Trend difference (wastewater slope minus admissions slope)",
-      "log_diff_ww_hosp" = "Prediction difference (ww-informed vs admissions-only)",
-      "rel_crps" = "Relative CRPS"
-    )
+      name = trend_diff_metric_names,
+      command = c(
+          "global_slope_hosp" = "Admissions trend (global slope)",
+          "global_slope_ww" = "Wastewater trend (global slope)",
+          "sd_slope_ww" = "Wastewater trend variability (slope s.d.)",
+          "diff_slope_ww_hosp" = "Trend difference (wastewater slope minus admissions slope)",
+          "log_diff_ww_hosp" = "Prediction difference (ww-informed vs admissions-only)",
+          "rel_crps" = "Relative CRPS")
   ),
   tar_map(
     tidyr::crossing(
@@ -1420,11 +1419,9 @@ trend_analysis_targets <- list(
         color = "black",
         alpha = 0.5
       ) +
-        labs(
-          x = trend_diff_metric_names[[trend_metric]],
-          y = trend_diff_metric_names[[diff_metric]],
-          fill = trend_diff_metric_names[[fill_metric]]
-        )
+          ggplot2::labs(x = trend_diff_metric_names[[trend_metric]],
+                        y = trend_diff_metric_names[[diff_metric]],
+                        fill = trend_diff_metric_names[[fill_metric]])
     ),
     tar_target(
       name = save_fig_trend_diff_scatter,
