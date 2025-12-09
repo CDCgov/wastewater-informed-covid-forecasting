7523515b7de0c3684700ba94c735ab15eb0fb6c3
Dylan H. Morris <dzl1@cdc.gov>
2025-12-08 22:38:28 -0500

    Line grouping

--- wweval/R/plot_calibration.R
+++ wweval/R/plot_calibration.R
@@ -146,7 +146,8 @@ forecast_interval_coverage_plot <- function(
       shape = .data$model
     )
   ) +
-      geom_line(linetype = "dashed") +
+      geom_line(aes(group = .data$model),
+                linetype = "dashed") +
       geom_point() +
     geom_hline(
       aes(yintercept = .data$interval_range / 100),
