e3ca0187ff9ddf9c3b333579ed8a88b45d3d1640
Dylan H. Morris <dzl1@cdc.gov>
2025-12-08 21:52:35 -0500

    Trend diff axis labels

--- _targets.R
+++ _targets.R
@@ -1368,6 +1368,16 @@ trend_analysis_targets <- list(
           global_slope_hosp_normed
       )
   ),
+  tar_target(
+      name = trend_diff_metric_names,
+      command = c(
+          "global_slope_hosp" = "Admissions trend (global slope)",
+          "global_slope_ww" = "Wastewater trend (global slope)",
+          "sd_slope_ww" = "Wastewater trend variability (slope s.d.)",
+          "diff_slope_ww_hosp" = "Trend difference (wastewater slope minus admissions slope)",
+          "log_diff_ww_hosp" = "Prediction difference (ww-informed vs admissions-only)",
+          "rel_crps" = "Relative CRPS")
+  ),
   tar_map(
     tidyr::crossing(
       tibble::tibble(
@@ -1408,7 +1418,10 @@ trend_analysis_targets <- list(
         size = 3,
         color = "black",
         alpha = 0.5
-      )
+      ) +
+          labs(x = trend_diff_metric_names[[trend_metric]],
+               y = trend_diff_metric_names[[diff_metric]],
+               fill = trend_diff_metric_names[[fill_metric]])
     ),
     tar_target(
       name = save_fig_trend_diff_scatter,
