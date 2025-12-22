a9cbc55ef2e5622702266fd13ecf04544ffdd219
Dylan H. Morris <dzl1@cdc.gov>
2025-12-22 15:48:53 -0500

    Narrower ticks

--- wweval/R/plot_hub_comparison.R
+++ wweval/R/plot_hub_comparison.R
@@ -277,10 +277,9 @@ plot_hub_heatmap_relative_wis <- function(
     scale_fill_score_ratio(
       name = "Relative WIS",
       limits = forecasttools::sym_limits(
-        c(0.5, relative_scores$mean_scores_ratio, 2), # min span of 1/2, 2
-        transform = "log10"
-      ),
-      breaks = 2^(-4:4)
+        relative_scores$mean_scores_ratio,
+        transform = "log10"),
+      breaks = 1.5^(-4:4)
     ) +
     get_plot_theme(
       rotate_x_ticks = TRUE
