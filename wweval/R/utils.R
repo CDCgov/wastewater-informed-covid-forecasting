9c29791c6018a73aeaef8ed6f45693dea99a54e7
Dylan H. Morris <dzl1@cdc.gov>
2026-07-14 18:56:46 -0400

    DRY pathing in postprocess, add time extraction

--- wweval/R/utils.R
+++ wweval/R/utils.R
@@ -400,3 +400,46 @@ forecast_output_path <- function(
     location
   ))
 }
+
+
+#' Get filepath
+#' @description
+#' A function to return a character string for each item to either be saved or
+#' read in, based on the nesting that is specified in this function. To change
+#' the nesting, we will change this function
+#'
+#'
+#' @param output_subdir Character string to upper level directory where outputs
+#' live
+#' @param scenario string indicating the evaluation scenario
+#' @param forecast_date string indicating the forecast date in "YYYY-MM-DD"
+#' format
+#' @param model_type string indicating the type of model, either "ww" or "hosp"
+#'  for now but can be used for model comparison
+#' @param location string indicating the location
+#' @param output_type string indicating the type of output
+#' @param file_extension string indicating the file extension e.g. ".tsv"
+#'
+#' @return string of the full filepath
+#' @export
+#'
+get_filepath <- function(
+  output_subdir,
+  scenario,
+  forecast_date,
+  model_type,
+  location,
+  output_type,
+  file_extension
+) {
+  return(fs::path(
+    forecast_output_path(
+      output_subdir,
+      scenario,
+      forecast_date,
+      model_type
+    ),
+    output_type,
+    ext = file_extension
+  ))
+}
