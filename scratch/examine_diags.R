658d35311c19ec33b3fd794866730b2b7d6b4207
Dylan H. Morris <dzl1@cdc.gov>
2026-08-11 21:22:09 +0000

    Scratch raw runtime plot

--- scratch/examine_diags.R
+++ scratch/examine_diags.R
@@ -1,4 +1,24 @@
-visualize_clock_time <- function(chain_run_times) {
-  chain_run_times |>
-    dplyr::group_by
+devtools::load_all("wweval")
+
+targets::tar_load("chain_run_time")
+targets::tar_load("slowest_chain_run_time")
+
+visualize_clock_time <- function(chain_run_time) {
+  chain_run_time |>
+    ggplot(aes(
+      x = .data$model_type,
+      y = .data$slowest_total_s / 60,
+      fill = .data$model_type
+    )) +
+    ggdist::stat_halfeye(
+      shape = 21,
+      point_size = 10,
+      interval_size_range = c(2, 5)
+    ) +
+    scale_y_continuous(transform = "log10") +
+    get_plot_theme() +
+    scale_fill_model() +
+    ylab("Slowest chain run time (m)")
 }
+
+visualize_clock_time(slowest_chain_run_time)
