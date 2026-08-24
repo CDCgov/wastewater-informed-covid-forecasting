c42cb450367fffb2d4b12fa70a2e9415c441811b
Dylan H. Morris <dzl1@cdc.gov>
2026-08-24 20:10:50 +0000

    Subplots filtered to comparisons

--- scratch/examine_diags.R
+++ scratch/examine_diags.R
@@ -2,13 +2,15 @@ devtools::load_all("wweval")
 
 targets::tar_load("chain_run_time")
 targets::tar_load("slowest_chain_run_time")
+targets::tar_load("diagnostic_extrema_preds_scored")
 targets::tar_load("granular_ww_metadata_used")
+targets::tar_load("date_locs_to_compare_retro")
 
-visualize_clock_time <- function(chain_run_time) {
-  chain_run_time |>
+.model_type_density_plot <- function(df, y) {
+  df |>
     ggplot(aes(
       x = .data$model_type,
-      y = .data$slowest_total_s / 60,
+      y = .data[[y]],
       fill = .data$model_type
     )) +
     ggdist::stat_halfeye(
@@ -16,6 +18,19 @@ visualize_clock_time <- function(chain_run_time) {
       point_size = 10,
       interval_size_range = c(2, 5)
     ) +
+    get_plot_theme()
+}
+
+visualize_rhat <- function(diagnostic_extrema) {
+  diagnostic_extrema |>
+    .model_type_density_plot("max_rhat") +
+    scale_y_continuous(transform = "log10")
+}
+
+visualize_clock_time <- function(chain_run_time) {
+  chain_run_time |>
+    dplyr::mutate(slowest_chain_time_m = .data$slowest_total_s / 60) |>
+    .model_type_density_plot("slowest_chain_time_m") +
     scale_y_continuous(transform = "log10") +
     get_plot_theme() +
     scale_fill_model() +
@@ -23,6 +38,13 @@ visualize_clock_time <- function(chain_run_time) {
 }
 
 visualize_clock_time(slowest_chain_run_time)
+visualize_clock_time(
+  slowest_chain_run_time |>
+    dplyr::inner_join(
+      date_locs_to_compare_retro,
+      by = c("location", "forecast_date")
+    )
+)
 
 clock_time_versus_sites <- function(clock_time, metadata) {
   data <- metadata |>
@@ -63,4 +85,15 @@ clock_time_versus_sites <- function(clock_time, metadata) {
     get_plot_theme()
 }
 
-clock_time_versus_sites(slowest_chain_run_time, granular_ww_metadata_used)
+clock_time_versus_sites(slowest_chain_run_time, granular_ww_metadata_used) +
+  scale_x_continuous(transform = "log10")
+
+clock_time_versus_sites(
+  slowest_chain_run_time |>
+    dplyr::inner_join(
+      date_locs_to_compare_retro,
+      by = c("forecast_date", "location")
+    ),
+  granular_ww_metadata_used
+) +
+  scale_x_continuous(transform = "log10")
