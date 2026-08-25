f06727a7cc97d707240a3f229dffbc76190d19cb
Dylan H. Morris <dzl1@cdc.gov>
2026-08-25 13:15:25 +0000

    Working demo plots

--- scratch/prior_posterior.R
+++ scratch/prior_posterior.R
@@ -1,32 +1,41 @@
 devtools::load_all('wweval')
-targets::tar_load('params')
+
+params <- RcppTOML::parseTOML("input/params.toml") |>
+  purrr::list_flatten(name_spec = "{inner}")
+
+loader <- get_object_loader(
+  "WA",
+  "2023-10-16",
+  "status_quo",
+  "output/eval_latest/raw_output"
+)
+
+example_fit <- loader("ww_fit_obj")
 
 to_plot <- list(
   list(
     "name" = "log10 genomes shed per infected individual",
-    "symbol" = "$\log_{10}(G)$",
+    "symbol" = "$\\log_{10}(G)$",
     "stan_name" = "log10_g",
-    "prior_pdf" = purrr::partial(
-      dnorm,
+    "prior_pdf" = distributional::dist_normal(
       mean = params$log10_g_prior_mean,
       sd = params$log10_g_prior_sd
     )
   ),
   list(
     "name" = "infection feedback strength",
-    "symbol" = "$\gamma$",
+    "symbol" = "$\\gamma$",
     "stan_name" = "infection_feedback",
-    "prior_pdf" = purrr::partial(
-      dlnorm,
-      meanlog = params$infection_feedback_prior_logmean,
-      sdlog = params$infection_feedback_prior_logsd
+    "prior_pdf" = distributional::dist_lognormal(
+      mu = params$infection_feedback_prior_logmean,
+      sigma = params$infection_feedback_prior_logsd
     )
   ),
   list(
     name = "Observation noise sd variability by site",
-    symbol = "$\sigma_{\log \sigma_{c}}$",
+    symbol = "$\\sigma_{\\log \\sigma_{c}}$",
     stan_name = "sd_log_sigma_ww_site",
-    prior_pdf = purrr::partial(
+    prior_pdf = distributional::dist_normal(
       dnorm,
       mean = params$sd_log_sigma_ww_site_prior_mode,
       sd = params$sd_log_sigma_ww_site_prior_sd
@@ -34,22 +43,110 @@ to_plot <- list(
   ),
   list(
     "name" = "sd of log site-lab multipliers",
-    "symbol" = "$\sigma_m$",
+    "symbol" = "$\\sigma_m$",
     "stan_name" = "ww_site_mod_sd",
-    prior_pdf = purrr::partial(
-      dnorm,
+    prior_pdf = distributional::dist_normal(
       mean = 0,
       sd = params$ww_site_mod_sd_sd
     )
   ),
   list(
     "name" = "s.d. of the AR(1) process on the R(t) differences",
-    "symbol" = "$\sigma_r$",
+    "symbol" = "$\\sigma_r$",
     "stan_name" = "eta_sd",
-    prior_pdf = purrr:partial(
-      dnorm,
+    prior_pdf = distributional::dist_normal(
       mean = params$eta_sd_mean,
       sd = params$eta_sd_sd
     )
   )
 )
+
+needed_params <- purrr::map_chr(to_plot, \(x) x$stan_name)
+
+draws <- tidybayes::spread_draws(
+  example_fit$fit$result,
+  log10_g,
+  infection_feedback,
+  sd_log_sigma_ww_site,
+  ww_site_mod_sd,
+  eta_sd
+)
+
+
+plot <- ggplot() +
+  ggdist::stat_slab(
+    mapping = aes(
+      xdist = distributional::dist_normal(
+        mean = params$log10_g_prior_mean,
+        sd = params$log10_g_prior_sd
+      )
+    ),
+    color = "black",
+    alpha = 0.25,
+    linetype = "dashed",
+    fill = "black"
+  ) +
+  ggdist::stat_slabinterval(
+    mapping = aes(x = .data$log10_g),
+    data = draws,
+    density = "histogram",
+    fill = "darkblue"
+  ) +
+  get_plot_theme()
+
+draws <- tidybayes::spread_draws(
+  example_fit$fit$result,
+  log10_g,
+  infection_feedback,
+  sd_log_sigma_ww_site,
+  ww_site_mod_sd,
+  eta_sd
+)
+
+plot <- ggplot() +
+  ggdist::stat_slab(
+    mapping = aes(
+      xdist = distributional::dist_lognormal(
+        mu = params$infection_feedback_prior_logmean,
+        sigma = params$infection_feedback_prior_logsd
+      )
+    ),
+    color = "black",
+    alpha = 0.25,
+    linetype = "dashed",
+    fill = "black"
+  ) +
+  ggdist::stat_slabinterval(
+    mapping = aes(x = .data$infection_feedback),
+    data = draws,
+    density = "histogram",
+    fill = "darkblue"
+  ) +
+  get_plot_theme()
+
+
+ggsave("~/example_plot_inf_feedback.png", plot)
+
+plot_prior_posterior <- function(draws_wide, param_name, dist) {
+  p <- ggplot() +
+    ggdist::stat_slab(
+      mapping = aes(xdist = dist),
+      color = "black",
+      alpha = 0.25,
+      linetype = "dashed",
+      fill = "black"
+    ) +
+    ggdist::stat_slabinterval(
+      mapping = aes(x = .data[[param_name]]),
+      data = draws,
+      density = "histogram",
+      fill = "darkblue"
+    ) +
+    get_plot_theme()
+
+  return(p)
+}
+
+plots <- purrr::map(to_plot, \(x) {
+  plot_prior_posterior(draws, x$stan_name, x$prior_pdf)
+})
