ae1bea05ee53faa7e97b308e56613cbbb213ca5d
Dylan H. Morris <dzl1@cdc.gov>
2026-09-26 14:50:56 -0400

    Tidyeval-proof parse_real_time_exclusions.R

--- wweval/R/parse_real_time_exclusions.R
+++ wweval/R/parse_real_time_exclusions.R
@@ -71,7 +71,7 @@ compute_retro_exclusions <- function(
       .data$exclusion %in% c("manual_exclude_ww", "manual_exclude_both")
     )
   data_quality_exclusions <- ww_data_quality_table |>
-    dplyr::filter(!.data$ww_sufficient) |>
+    dplyr::filter_out(.data$ww_sufficient) |>
     dplyr::select("forecast_date", "location", exclusion = "status")
 
   convergence_exclusions <- convergence_table |>
@@ -91,7 +91,7 @@ compute_retro_exclusions <- function(
     convergence_exclusions
   ) |>
     order_col("exclusion", levels = exclusion_hierarchy) |>
-    dplyr::arrange(exclusion) |>
+    dplyr::arrange(.data$exclusion) |>
     dplyr::distinct(.data$forecast_date, .data$location, .keep_all = TRUE)
 
   return(result)
